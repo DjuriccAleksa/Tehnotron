7efa9d33e431f4eb4dd7d9eda74b3c5e59f53e50
DjuriccAleksa <djuric258@gmail.com>
2023-07-21 21:06:18 +0200

    init

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {SafeAreaView, View} from 'react-native';
+
+const App = () => {
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
